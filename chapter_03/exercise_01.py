82cb5b3cb2bf10cf95c32afbaff734397108ff71
hackzera <aland295@gmail.com>
2022-02-09 20:40:55 -0300

    exercices chapter 2 and 3

--- chapter_03/exercise_01.py
+++ chapter_03/exercise_01.py
@@ -1,25 +1,24 @@
 #rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours
-try:
-    hours = input('Enter hours: ')
-    rate = input('Enter rate: ')
-    
-    if float(hours)>40:
-        extra_hour = float(hours) - 40
-        new_rate = rate*1.5
-        value_of_payrate = extra_hour * float(new_rate)
+hours = input('Enter hours: ')
+rate = input('Enter rate: ')
 
-        normal_hour = 40
-        payrate = (float(normal_hour) * float(rate))
 
-        new_payrate = value_of_payrate + payrate
+if float(hours)>40:
+    extra_hour = float(hours) - 40
+    new_rate = rate*1.5
+    value_of_payrate = extra_hour * float(new_rate)
 
-        print('your pay rate is: ',new_payrate)
+    normal_hour = 40
+    payrate = (float(normal_hour) * float(rate))
 
-    else:
-        payrate = (float(hours) * float(rate))
-        print('your pay rate is: ',payrate)
-except:
-    print('Please enter a numeric number')
+    new_payrate = value_of_payrate + payrate
+
+    print('your pay rate is: ',new_payrate)
+
+
+else:
+    payrate = (float(hours) * float(rate))
+    print('your pay rate is: ',payrate)
 
 
 
--- chapter_03/exercise_02.py
+++ chapter_03/exercise_02.py
@@ -1,22 +1,23 @@
 #rewrite your pay program using try and except so that your program handles non-numeric
 #input gracefully by printing a message and exiting the program. 
-hours = input('Enter hours: ')
-rate = input('Enter rate: ')
+try:
+    hours = input('Enter hours: ')
+    rate = input('Enter rate: ')
+    
+    if float(hours)>40:
+        extra_hour = float(hours) - 40
+        new_rate = rate*1.5
+        value_of_payrate = extra_hour * float(new_rate)
 
+        normal_hour = 40
+        payrate = (float(normal_hour) * float(rate))
 
-if float(hours)>40:
-    extra_hour = float(hours) - 40
-    new_rate = rate*1.5
-    value_of_payrate = extra_hour * float(new_rate)
+        new_payrate = value_of_payrate + payrate
 
-    normal_hour = 40
-    payrate = (float(normal_hour) * float(rate))
+        print('your pay rate is: ',new_payrate)
 
-    new_payrate = value_of_payrate + payrate
-
-    print('your pay rate is: ',new_payrate)
-
-
-else:
-    payrate = (float(hours) * float(rate))
-    print('your pay rate is: ',payrate)
+    else:
+        payrate = (float(hours) * float(rate))
+        print('your pay rate is: ',payrate)
+except:
+    print('Please enter a numeric number')
